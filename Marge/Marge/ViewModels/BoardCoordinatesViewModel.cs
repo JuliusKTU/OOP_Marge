0e8298a3822a3a96476c7a63e464682a6005f7cc
JuliusKTU <38457466+JuliusKTU@users.noreply.github.com>
2020-11-08 20:19:00 +0200

    score

--- Marge/Marge/ViewModels/BoardCoordinatesViewModel.cs
+++ Marge/Marge/ViewModels/BoardCoordinatesViewModel.cs
@@ -320,7 +320,7 @@ namespace Marge.ViewModels
                     SetGamePause();
                 }
 
-                if (MainPlayer.Score >= 10)
+                if (MainPlayer.Score >= 200)
                 {
                     MainPlayer.SendGameOverMessage(_chatService, UniqueID);
                     MainPlayer.Score = 0;
